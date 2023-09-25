b8a016ef13e2b8ba98d388da60b0cd4b7b761746
partho-junegaming <partho@supergaming.com>
2023-09-25 15:56:19 +0530

    11

---
+++ index.js
@@ -0,0 +1,36 @@
+const core = require("@actions/core")
+const github = require("@actions/github")
+const exec = require("@actions/exec")
+// import * as core from "@actions/core";
+// import * as github from "@actions/github";
+// import * as exec from "@actions/exec";
+
+
+try {
+    // 'who-to-greet' input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // exec dir command
+    // let dirOutput = '';
+    // let dirError = '';
+    // const options = {
+    //     listeners: {
+    //         stdout: (data) => {
+    //             dirOutput += data.toString();
+    //         }, 
+    //         stderr: (data) => {
+    //             dirError += data.toString();
+    //         }
+    //     }
+    // };
+    // await exec.exec('dir', [], options);
+    // console.log(dirOutput);
+    // console.log(dirError);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+} catch(error) {
+    core.setFailed(error.message);
+}
